7f096606ea73b4395050e88ea798c07ad72fb6cf
hischan12 <hischan12@handong.ac.kr>
2024-11-21 22:23:04 +0900

    find mate added

---
+++ lib/findMate.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class FindMatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          '나의 메이트 찾기',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Color(0xFF49454F),
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Container(
+              width: 24,
+              height: 24,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(),
+              child: Icon(
+                Icons.location_on,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _buildCategoryChip('치킨'),
+                      _buildCategoryChip('피자'),
+                      _buildCategoryChip('햄버거'),
+                      _buildCategoryChip('떡볶이'),
+                    ],
+                  ),
+                  _buildMatePost(
+                    title: '야식팟 모집중~',
+                    description: '카벤에서 같이 꼬꼬뽀끼 먹을 분들 구해요!',
+                    currentPeople: '7/10',
+                  ),
+                  _buildMatePost(
+                    title: '치킨 메이트 찾아요',
+                    description: '치코파닭 먹으면서 친해지고 싶어요!',
+                    currentPeople: '2/5',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryChip(String label) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: Color(0xFFD9D9D9),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(label, style: TextStyle(fontSize: 12)),
+    );
+  }
+
+  Widget _buildMatePost({
+    required String title,
+    required String description,
+    required String currentPeople,
+  }) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 8),
+              Text(
+                description,
+                style: TextStyle(fontSize: 13, color: Colors.grey),
+              ),
+            ],
+          ),
+          Text(currentPeople, style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'findMate.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,17 +26,23 @@ class HomePage extends StatelessWidget {
             runSpacing: 20,
             children: [
               _buildLottieCircle(
-                  "https://lottie.host/38977bd0-40ec-4a0a-b57e-6b5ab196c761/Lp9GuVzlsK.json"),
+                  "https://lottie.host/38977bd0-40ec-4a0a-b57e-6b5ab196c761/Lp9GuVzlsK.json",
+                  context),
               _buildLottieCircle(
-                  "https://lottie.host/bfc93f77-20a0-4f4b-9d87-4242beaad756/HqbFaYRD21.json"),
+                  "https://lottie.host/bfc93f77-20a0-4f4b-9d87-4242beaad756/HqbFaYRD21.json",
+                  context),
               _buildLottieCircle(
-                  "https://lottie.host/e3d0f572-cc1a-47a2-9712-9c6a892fe684/kBtkxl7hoi.json"),
+                  "https://lottie.host/e3d0f572-cc1a-47a2-9712-9c6a892fe684/kBtkxl7hoi.json",
+                  context),
               _buildLottieCircle(
-                  "https://lottie.host/663b3be2-3e78-4a34-b804-00d1ec4e7d34/EPoCzQGDM4.json"),
+                  "https://lottie.host/663b3be2-3e78-4a34-b804-00d1ec4e7d34/EPoCzQGDM4.json",
+                  context),
               _buildLottieCircle(
-                  "https://lottie.host/057124e7-20d3-49c9-9f9f-39aff106d9d7/yG4BgeBHOU.json"),
+                  "https://lottie.host/057124e7-20d3-49c9-9f9f-39aff106d9d7/yG4BgeBHOU.json",
+                  context),
               _buildLottieCircle(
-                  "https://lottie.host/be08b500-e95e-4ca3-b993-6b021bfb1abc/K0L7LBexhS.json"),
+                  "https://lottie.host/be08b500-e95e-4ca3-b993-6b021bfb1abc/K0L7LBexhS.json",
+                  context),
             ],
           ),
         ],
@@ -43,33 +50,43 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildLottieCircle(String lottieUrl) {
-    return ClipOval(
-      child: Container(
-        width: 120,
-        height: 120,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 10,
-              offset: const Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Lottie.network(
-          lottieUrl,
-          fit: BoxFit.cover,
-          repeat: true,
-          animate: true,
-          errorBuilder: (context, error, stackTrace) {
-            return const Icon(
-              Icons.error,
-              color: Colors.red,
-              size: 40,
-            );
-          },
+  Widget _buildLottieCircle(String lottieUrl, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FindMatePage(),
+          ),
+        );
+      },
+      child: ClipOval(
+        child: Container(
+          width: 120,
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Lottie.network(
+            lottieUrl,
+            fit: BoxFit.cover,
+            repeat: true,
+            animate: true,
+            errorBuilder: (context, error, stackTrace) {
+              return const Icon(
+                Icons.error,
+                color: Colors.red,
+                size: 40,
+              );
+            },
+          ),
         ),
       ),
     );
