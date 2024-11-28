5f5bd7ca974e911b7d2418a2ab79394dbd1b9caf
orineoguri02 <gunwoo5285@gmail.com>
2024-11-28 22:23:50 +0900

    이것임

--- lib/HomePage/chat.dart
+++ lib/HomePage/chat.dart
@@ -18,12 +18,15 @@ class _ChatScreenState extends State<ChatScreen> {
   User? loggedInUser;
   String? nickName;
   String? title;
+  int maxParticipants = 10; // Limit the number of participants
+  bool isRoomFull = false; // Track if the room is full
 
   @override
   void initState() {
     super.initState();
     getCurrentUser();
     getCategoryTitle();
+    checkRoomCapacity();
   }
 
   Future<void> getCurrentUser() async {
@@ -71,6 +74,42 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  Future<void> checkRoomCapacity() async {
+    try {
+      final docSnapshot = await FirebaseFirestore.instance
+          .collection(widget.category)
+          .doc(widget.id)
+          .get();
+
+      if (docSnapshot.exists) {
+        int currentCount = docSnapshot.data()?['currentCount'] ?? 0;
+        if (currentCount >= maxParticipants) {
+          setState(() {
+            isRoomFull = true;
+          });
+        } else {
+          await FirebaseFirestore.instance
+              .collection(widget.category)
+              .doc(widget.id)
+              .update({'currentCount': FieldValue.increment(1)});
+        }
+      }
+    } catch (e) {
+      print("Error checking room capacity: $e");
+    }
+  }
+
+  Future<void> decrementParticipantCount() async {
+    try {
+      await FirebaseFirestore.instance
+          .collection(widget.category)
+          .doc(widget.id)
+          .update({'currentCount': FieldValue.increment(-1)});
+    } catch (e) {
+      print("Error decrementing participant count: $e");
+    }
+  }
+
   void _sendMessage() {
     _controller.text = _controller.text.trim();
     if (_controller.text.isNotEmpty &&
@@ -93,143 +132,173 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  Future<bool> _onWillPop() async {
+    await decrementParticipantCount(); // Decrement count when back button is pressed
+    return true; // Allow the back navigation
+  }
+
+  @override
+  void dispose() {
+    // Decrement participant count when the screen is disposed
+    decrementParticipantCount();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          title ?? 'Chat',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    if (isRoomFull) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Room Full'),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              _auth.signOut();
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Colors.white, Color(0XFFFFBAB6)],
+        body: Center(
+          child: Text(
+            'This room is full. Please try again later.',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
           ),
         ),
-        child: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection(widget.category)
-                    .doc(widget.id)
-                    .collection('messages')
-                    .orderBy('timestamp', descending: true)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return Center(child: Text("No messages yet."));
-                  }
-                  final chatDocs = snapshot.data!.docs;
-                  return ListView.builder(
-                    reverse: true,
-                    itemCount: chatDocs.length,
-                    itemBuilder: (ctx, index) {
-                      bool isMe = chatDocs[index]['sender'] == nickName;
-                      return Column(
-                        crossAxisAlignment: isMe
-                            ? CrossAxisAlignment.end
-                            : CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              chatDocs[index]['sender'],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey[700],
-                              ),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: isMe
-                                ? MainAxisAlignment.end
-                                : MainAxisAlignment.start,
-                            children: [
-                              Container(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 10, horizontal: 16),
-                                margin: EdgeInsets.symmetric(
-                                    vertical: 4, horizontal: 8),
-                                decoration: BoxDecoration(
-                                  color: isMe
-                                      ? Colors.grey[100]
-                                      : Color(0XFFD76F69),
-                                  borderRadius: isMe
-                                      ? BorderRadius.only(
-                                          topLeft: Radius.circular(14),
-                                          topRight: Radius.circular(14),
-                                          bottomLeft: Radius.circular(14),
-                                        )
-                                      : BorderRadius.only(
-                                          topLeft: Radius.circular(14),
-                                          topRight: Radius.circular(14),
-                                          bottomRight: Radius.circular(14),
-                                        ),
-                                ),
-                                child: Text(
-                                  chatDocs[index]['text'],
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _controller,
-                      cursorColor: Color(0XFFC5524C),
-                      decoration: InputDecoration(
-                        hintText: 'Send a message...',
-                        hintStyle: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0XFF4E5968),
-                        ),
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.send,
-                      color: Color(0XFF4E5968),
-                    ),
-                    onPressed: _sendMessage,
-                  ),
-                ],
-              ),
+      );
+    }
+
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            title ?? 'Chat',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                _auth.signOut();
+                Navigator.pop(context);
+              },
             ),
           ],
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.white, Color(0XFFFFBAB6)],
+            ),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection(widget.category)
+                      .doc(widget.id)
+                      .collection('messages')
+                      .orderBy('timestamp', descending: true)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return Center(child: Text("No messages yet."));
+                    }
+                    final chatDocs = snapshot.data!.docs;
+                    return ListView.builder(
+                      reverse: true,
+                      itemCount: chatDocs.length,
+                      itemBuilder: (ctx, index) {
+                        bool isMe = chatDocs[index]['sender'] == nickName;
+                        return Column(
+                          crossAxisAlignment: isMe
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Text(
+                                chatDocs[index]['sender'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: isMe
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 16),
+                                  margin: EdgeInsets.symmetric(
+                                      vertical: 4, horizontal: 8),
+                                  decoration: BoxDecoration(
+                                    color: isMe
+                                        ? Colors.grey[100]
+                                        : Color(0XFFD76F69),
+                                    borderRadius: isMe
+                                        ? BorderRadius.only(
+                                            topLeft: Radius.circular(14),
+                                            topRight: Radius.circular(14),
+                                            bottomLeft: Radius.circular(14),
+                                          )
+                                        : BorderRadius.only(
+                                            topLeft: Radius.circular(14),
+                                            topRight: Radius.circular(14),
+                                            bottomRight: Radius.circular(14),
+                                          ),
+                                  ),
+                                  child: Text(
+                                    chatDocs[index]['text'],
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        cursorColor: Color(0XFFC5524C),
+                        decoration: InputDecoration(
+                          hintText: 'Send a message...',
+                          hintStyle: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0XFF4E5968),
+                          ),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 20),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.send,
+                        color: Color(0XFF4E5968),
+                      ),
+                      onPressed: _sendMessage,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
