548d76f9f45146606bed6b5b46aaf01c5b4b0710
hischan12 <hischan12@handong.ac.kr>
2024-11-30 17:09:47 +0900

    ai 추천

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,8 +1,8 @@
 import 'package:envied/envied.dart';
 part 'env.g.dart';
 
-@Envied(path: ".env") // .env 파일에서 환경 변수를 로드
+@Envied(path: ".env")
 abstract class Env {
-  @EnviedField(varName: 'OPEN_AI_API_KEY') // .env 파일에 있는 OPEN_AI_API_KEY를 참조
-  static const String apiKey = _Env.apiKey; // _Env 클래스에서 apiKey를 가져옴
+  @EnviedField(varName: 'OPEN_AI_API_KEY')
+  static const String apiKey = _Env.apiKey;
 }
--- lib/env/env.g.dart
+++ lib/env/env.g.dart
@@ -9,6 +9,5 @@ part of 'env.dart';
 // coverage:ignore-file
 // ignore_for_file: type=lint
 final class _Env {
-  static const String apiKey =
-      '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';
+  static const String apiKey = '요기에 api 넣기';
 }
